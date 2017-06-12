0d6da674bbb9249d5058a8dc234a34af4b0e9b3e
Nicholas Devenish <ndevenish@gmail.com>
2017-06-12 14:04:45 +0100

    Initial package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mtz',
+      version='0.1',
+      description='Simple mtz-reading library',
+      author='Nicholas Devenish',
+      author_email='ndevenish@gmail.com',
+      packages=['mtz'])
